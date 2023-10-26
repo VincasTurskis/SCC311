bd9bdd7e6ec966d3b3d1d07c4e663cd2c53a8f42
Vincas Turskis <v.turskis@lancaster.ac.uk>
2023-10-26 03:04:09 +0200

    Added seller side functionality to the server

--- coursework1/SharedObjects/AuctionItem.java
+++ coursework1/SharedObjects/AuctionItem.java
@@ -1,45 +1,84 @@
 import java.io.Serializable;
+import java.util.List;
+import java.util.LinkedList;
 
 public class AuctionItem implements Serializable {
     private int _itemId;
     private String _itemTitle;
     private String _itemDescription;
-    private int _reservePrice;
-    private int _currentBidPrice;
-    private int _currentBuyerId;
+    private float _reservePrice;
+    private float _currentBidPrice;
+    private List<Bid> _bidHistory; // a list that stores all bids, current and previous, on the item.
+                                    // Possible use is recovery if highest bid on closing is invalid
 
-    public AuctionItem(int id, String title, String desc, int startingPrice, int reservePrice)
+    public AuctionItem(int id, String title, String desc, float startingPrice, float reservePrice)
     {
+        _bidHistory = new LinkedList<Bid>();
         _itemId = id;
         _itemTitle = title;
         _itemDescription = desc;
         _reservePrice = reservePrice;
         _currentBidPrice = startingPrice;
-        _currentBuyerId = -1;
+
     }
     public String print()
     {
         String s ="\n   ID: " + _itemId + "\n" +
                     "   Title: " + _itemTitle + "\n" +
-                    "   Description: " + _itemDescription;
+                    "   Description: " + _itemDescription + "\n";
         return s;
     }
     public int getId()
     {
         return _itemId;
     }
-    public int getCurrentBuyerId()
+    public String getTitle()
     {
-        return _currentBuyerId;
+        return _itemTitle;
     }
-    public void newBid(int newPrice, int newBuyerId)
+    public String getDescription()
     {
-        if(newPrice <= _currentBidPrice) return;
-        _currentBidPrice = newPrice;
-        _currentBuyerId = newBuyerId;
+        return _itemDescription;
     }
-    public int getReservePrice()
+    public float getReservePrice()
     {
         return _reservePrice;
+    }
+    public String getHighestBidName()
+    {
+        if(_bidHistory == null || _bidHistory.size() == 0) return "No bid";
+        return _bidHistory.get(_bidHistory.size() - 1).bidderName;
+    }
+    public String getHighestBidEmail()
+    {
+        if(_bidHistory == null || _bidHistory.size() == 0) return "No bid";
+        return _bidHistory.get(_bidHistory.size() - 1).bidderEmail;
+    }
+    public float getHighestBidAmount()
+    {
+        if(_bidHistory == null || _bidHistory.size() == 0) return -1;
+        return _bidHistory.get(_bidHistory.size() - 1).bidPrice;
+    }
+    public String newBid(int newPrice, String newBuyerName, String newBuyerEmail)
+    {
+        
+        if(newPrice <= _currentBidPrice) return "Bid failed: proposed price is lower than current price.";
+        _currentBidPrice = newPrice;
+        Bid newBid = new Bid(newPrice, newBuyerName, newBuyerEmail);
+        _bidHistory.add(newBid);
+        return "Bid succeeded.";
+
+    }
+    private class Bid
+    {
+        public float bidPrice;
+        public String bidderName;
+        public String bidderEmail;
+        public Bid(float price, String name, String email)
+        {
+            bidderName = name;
+            bidderEmail = email;
+            bidPrice = price;
+        }
     }
 }
--- coursework1/server/Server.java
+++ coursework1/server/Server.java
@@ -25,11 +25,53 @@ public class Server implements IRemoteAuction{
 
     public int createAuction(String title, String description, int startingPrice, int reservePrice)
     {
-        if(title == null || description == null || startingPrice < 0 || reservePrice <= startingPrice) return -1;
+        System.out.print("Server: attempting to create auction listing for " + title + "...");
+        if(title == null || description == null || startingPrice < 0 || reservePrice <= startingPrice)
+        {
+            System.out.println("error, bad arguments");
+            return -1;
+        }
         AuctionItem newItem = new AuctionItem(nextId, title, description, startingPrice, reservePrice);
         auctionItems.put(nextId, newItem);
         nextId++;
+        System.out.println("success");
         return newItem.getId();
+    }
+
+    public String closeAuction(int auctionId)
+    {
+        System.out.print("Server: attempting to close auction for ID: " + auctionId + "...");
+        String result;
+        if(auctionItems == null || auctionItems.get(auctionId) == null)
+        {
+            result = "Item does not exist";
+            System.out.println("error: " + result);
+            return result;
+        }
+        AuctionItem toClose = auctionItems.get(auctionId);
+        auctionItems.remove(auctionId);
+
+        if(toClose.getHighestBidAmount() < toClose.getReservePrice())
+        {
+            result = "Reserve price was not reached";
+            System.out.println("error: " + result);
+            return result;
+        }
+        if(toClose.getHighestBidName() == "No bid")
+        {
+            result = "There were no bidders for this item";
+            System.out.println("error: " + result);
+            return result;
+        }
+        String name, email, amountString;
+        float amount;
+        name = toClose.getHighestBidName();
+        email = toClose.getHighestBidEmail();
+        amount = toClose.getHighestBidAmount();
+        amountString = String.format("%.02f", amount);
+        result = "Auction for item ID:" + auctionId + "closed. The winner is " + name + " (" + email + ") with an amount of " + amountString;
+        System.out.println("success");
+        return result;
     }
 
     public static void main(String[] args) {
