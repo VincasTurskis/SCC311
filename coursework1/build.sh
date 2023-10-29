ec503e77a9b063b0126a67311ca615a5fff14ab9
Vincas Turskis <v.turskis@lancaster.ac.uk>
2023-10-29 09:50:48 +0100

    Added some seller client functionality

--- coursework1/build.sh
+++ coursework1/build.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
-find . -name '*.class' -delete
-cd SharedObjects
-javac *.java
-cd ..
+rm -rf ./bin
+cp -r ./src ./bin
+cd bin
+javac $(find ./* | grep .java)
+find . -type f -name '*.java' -delete
 cp SharedObjects/*.class client
 cp SharedObjects/*.class server
-cd server
-javac *.java
-cd ../client
-javac *.java
-cd ..
+cp SharedObjects/*.class buyerClient
+cp SharedObjects/*.class sellerClient
+cp interfaces/*.class server
+cp interfaces/*.class client
+cp interfaces/*.class sellerClient
+cp interfaces/*.class buyerClient
