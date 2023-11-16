3f2a98607ebe1562e10db80a12bab233de3b4694
Vincas Turskis <v.turskis@lancaster.ac.uk>
2023-11-16 12:20:40 +0100

    Started on double auction; moved auction items into server folder

--- coursework1/src/client/DoubleAuctionClient.java
+++ coursework1/src/client/DoubleAuctionClient.java
@@ -1,3 +1,24 @@
 public class DoubleAuctionClient {
-    
+    public static void run(Account currentAccount, InputProcessor input, IRemoteAuction server)
+    {
+        boolean actionLoop = true;
+        while(actionLoop)
+        {
+            // Display selection menu in terminal
+            System.out.println("Double Auction");
+            System.out.println("\nSelect an action:");
+            System.out.println("0. Back");
+            System.out.println("1. Show all listings");
+            System.out.println("2. Sell an item");
+            System.out.println("3. Buy an item");
+            // use the InputProcessor class to get the next int from terminal input
+            int option = input.ReadNextInt();
+            //check if input is correct
+            while(option < 0 || option > 3)
+            {
+                System.out.println("Error: input a number 0-3");
+                option = input.ReadNextInt();
+            }
+        }
+    }
 }
