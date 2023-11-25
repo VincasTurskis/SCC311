1fc6ace84e5ce1154189250efa3197d965b9b0a5
Vincas Turskis <v.turskis@lancaster.ac.uk>
2023-11-25 20:46:04 +0100

    Made attempts toward adding jgroups jar file

--- coursework1/build.sh
+++ coursework1/build.sh
@@ -3,7 +3,8 @@ rm */**/**.class
 mkdir -p bin
 cp -r ./src/* ./bin
 cd bin
-javac $(find ./* | grep .java)
+javac -cp "./jgroups-3.6.20.Final.jar";. $(find ./* | grep .java)
 find . -type f -name '*.java' -delete
 cp SharedObjects/*.class client
 cp SharedObjects/*.class server
+exec bash
